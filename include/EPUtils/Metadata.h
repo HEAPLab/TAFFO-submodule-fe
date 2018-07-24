1e01cfa74f30443ff9992963b6319a923cdd47ec
Michele Chiari <michele.chiari@polimi.it>
2018-07-24 23:03:21 +0200

    Metadata.h documented better.

--- include/EPUtils/Metadata.h
+++ include/EPUtils/Metadata.h
@@ -37,14 +37,22 @@
 namespace ErrorProp {
 
 /// Attach metadata containing the computed error to the given instruction.
-void setErrorMetadata(llvm::Instruction &, const AffineForm<inter_t> &);
+/// E is an affine form containing the error terms computed for Instruction I.
+void setErrorMetadata(llvm::Instruction &I, const AffineForm<inter_t> &E);
 
 /// Attach metadata containing ranges and initial absolute errors
 /// for each argument of the given function.
-void setFunctionArgsMetadata(llvm::Function &,
+/// RErr is an array/vector of pairs containing
+/// ranges and errors for function arguments.
+/// Each pair refers to the function parameter with the same index.
+/// The first member of each term contains its range,
+/// while the second is a 0-centered affine form containing the initial error:
+/// It can be created with AffineForm<inter_t>(0.0, Error),
+/// where the type of Error must be convertible to inter_t (defined in FixedPoint.h).
+void setFunctionArgsMetadata(llvm::Function &F,
 			     const llvm::ArrayRef<std::pair<
 			     const FixedPointValue *,
-			     const AffineForm<inter_t> *> >);
+			     const AffineForm<inter_t> *> > RErr);
 
 /// Extract function argument ranges and initial errors
 /// from Function metadata.
@@ -52,13 +60,18 @@ llvm::SmallVector<std::pair<FPInterval, AffineForm<inter_t> >, 1U>
 retrieveArgsRangeError(const llvm::Function &);
 
 /// Check whether error propagation must be performed on this function.
-bool propagateFunction(const llvm::Function &);
+/// Returns true if metadata labelled with FUNCTION_ARGS_METADATA
+/// is found attached to F.
+bool propagateFunction(const llvm::Function &F);
 
 /// Attach maximum error tolerance to Cmp instruction.
 /// The metadata are attached only if the comparison may be wrong.
 void setCmpErrorMetadata(llvm::Instruction &, const CmpErrorInfo &);
 
 /// Attach Range and Error metadata to global object V.
+/// Range contains the predicted range of V,
+/// and Error contains the initial absolute error.
+/// It may be created with AffineForm<inter_t>(0.0, Error).
 void setGlobalVariableMetadata(llvm::GlobalObject &V,
 			       const FixedPointValue *Range,
 			       const AffineForm<inter_t> *Error);
@@ -71,6 +84,8 @@ std::pair<FPInterval, AffineForm<inter_t> >
 retrieveGlobalVariableRangeError(const llvm::GlobalObject &V);
 
 /// Attach MaxRecursionCount to the given function.
+/// Attaches metadata containing the maximum number of recursive calls
+/// that is allowed for function F.
 void
 setMaxRecursionCountMetadata(llvm::Function &F, unsigned MaxRecursionCount);
 
@@ -80,7 +95,8 @@ unsigned
 retrieveMaxRecursionCount(const llvm::Function &F);
 
 /// Attach unroll count metadata to loop L.
-/// Attaches loop unroll metadata to the terminator of the loop header.
+/// Attaches UnrollCount as the number of times to unroll loop L
+/// as metadata to the terminator instruction of the loop header.
 void
 setLoopUnrollCountMetadata(llvm::Loop &L, unsigned UnrollCount);
 

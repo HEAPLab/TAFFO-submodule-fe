d06c902bac9625e57c4a39cef3a21a8ed192e63f
Daniele Cattaneo <daniele3.cattaneo@mail.polimi.it>
2018-09-12 14:59:01 +0200

    Add errorprop data to the results package

--- test/axbench/inversek2j/compile+collect.sh
+++ test/axbench/inversek2j/compile+collect.sh
@@ -12,4 +12,5 @@ make OX=-O3
 cp stats/main.fixp.mix.txt $1/inversej2k_out_ic_fix.txt
 cp stats/main.mix.txt $1/inversej2k_out_ic_float.txt
 cp stats/main.llvm.txt $1/inversej2k_out.txt
+cat stats/errorprop.log | grep 'Computed error for target' > $1/inversej2k_out_errorprop.txt
 

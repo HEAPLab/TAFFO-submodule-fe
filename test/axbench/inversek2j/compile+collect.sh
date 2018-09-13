0be9a67f7ac7371fdcd4f11696dc0876cf94f4b4
Daniele Cattaneo <daniele3.cattaneo@mail.polimi.it>
2018-09-13 15:07:28 +0200

    Fix name inconsistency in inversek2j

--- test/axbench/inversek2j/compile+collect.sh
+++ test/axbench/inversek2j/compile+collect.sh
@@ -7,10 +7,12 @@ if [[ $# -ne 1 ]]; then
   exit
 fi
 
+bench=inversek2j
+
 make clean
 make OX=-O3
-cp stats/main.fixp.mix.txt $1/inversej2k_out_ic_fix.txt
-cp stats/main.mix.txt $1/inversej2k_out_ic_float.txt
-cp stats/main.llvm.txt $1/inversej2k_out.txt
-cat stats/errorprop.log | grep 'Computed error for target' > $1/inversej2k_out_errorprop.txt
+cp stats/main.fixp.mix.txt $1/${bench}.txt
+cp stats/main.mix.txt $1/${bench}_out_ic_float.txt
+cp stats/main.llvm.txt $1/${bench}_out.txt
+cat stats/errorprop.log | grep 'Computed error for target' > $1/${bench}_out_errorprop.txt
 

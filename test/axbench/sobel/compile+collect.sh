01c2720d48992f11fd5e3c164e57041eb69be661
Daniele Cattaneo <daniele3.cattaneo@mail.polimi.it>
2018-09-15 00:15:52 +0200

    Enable errorprop log output

--- test/axbench/sobel/compile+collect.sh
+++ test/axbench/sobel/compile+collect.sh
@@ -14,4 +14,4 @@ make OX=-O3
 cp stats/main.fixp.mix.txt $1/${bench}_out_ic_fix.txt
 cp stats/main.mix.txt $1/${bench}_out_ic_float.txt
 cp stats/main.llvm.txt $1/${bench}_out.txt
-# cat stats/errorprop.log | grep 'Computed error for target' > $1/${bench}_out_errorprop.txt
+cat stats/errorprop.log | grep 'Computed error for target' > $1/${bench}_out_errorprop.txt

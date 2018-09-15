b30c320337b9e3ff724a156b4bb7f59c8770921e
Daniele Cattaneo <daniele3.cattaneo@mail.polimi.it>
2018-09-15 18:41:06 +0200

    Fix naming discrepancy in inversek2j

--- test/axbench/inversek2j/compile+collect.sh
+++ test/axbench/inversek2j/compile+collect.sh
@@ -11,7 +11,7 @@ bench=inversek2j
 
 make clean
 make OX=-O3
-cp stats/main.fixp.mix.txt $1/${bench}.txt
+cp stats/main.fixp.mix.txt $1/${bench}_out_ic_fix.txt
 cp stats/main.mix.txt $1/${bench}_out_ic_float.txt
 cp stats/main.llvm.txt $1/${bench}_out.txt
 cat stats/errorprop.log | grep 'Computed error for target' > $1/${bench}_out_errorprop.txt
